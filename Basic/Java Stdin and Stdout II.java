4de6e48af85c526aff580da08ff7bf71f6828026
Luigi <111696215+Helltaker1@users.noreply.github.com>
2024-03-06 18:33:57 +0100

    Create Java Stdin and Stdout II.java
    add solution to Create Java Stdin and Stdout II

---
+++ Basic/Java Stdin and Stdout II.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = Integer.parseInt(scan.nextLine());
+        double d = Double.parseDouble(scan.nextLine());
+        String s = scan.nextLine();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
